2da0ed4827d98169a18771a01b085a7b59228ec1
tomfr <tom.frances97@yahoo.fr>
2023-01-25 18:27:34 +0100

    correction definition api swagger

--- restAPI/src/main/java/com/g2/gromed/endpoint/ICommandeEndpoint.java
+++ restAPI/src/main/java/com/g2/gromed/endpoint/ICommandeEndpoint.java
@@ -1,11 +1,7 @@
 package com.g2.gromed.endpoint;
 
 import com.g2.gromed.model.dto.commande.PresentationPanierDTO;
-import com.g2.gromed.model.dto.presentation.PresentationCardDTO;
-import com.g2.gromed.model.dto.utilisateur.UtilisateurDTO;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,37 +12,25 @@ import java.util.List;
 @RequestMapping("/commande")
 public interface ICommandeEndpoint {
 	
-	@ApiOperation(value = "Ajouter une présentation au panier dans une certaine quantité",
-			response = PresentationCardDTO.class)
-	@ApiResponse(responseCode = "200", description = "OK",
-			content = @Content(mediaType = "application/json", schema = @Schema(implementation = PresentationPanierDTO.class)))
-	@ApiResponse(responseCode = "500", description = "Erreur serveur",
-			content = @Content(mediaType = "application/json"))
-	@ApiResponse(responseCode = "404", description = "Commande ou présentation non trouvé",
-			content = @Content(mediaType = "application/json"))
-	@PostMapping("/add")
+	@ApiOperation(value = "Ajouter une présentation au panier dans une certaine quantité")
+	@ApiResponse(responseCode = "200", description = "OK")
+	@ApiResponse(responseCode = "500", description = "Erreur serveur")
+	@ApiResponse(responseCode = "404", description = "Commande ou présentation non trouvé")
+	@PostMapping(value = "/add", produces = "application/json")
 	ResponseEntity<Integer> addPresentationToCart(@RequestParam("idUtilisateur") Long idUtilisateur, @RequestParam("codeCIP7") String codeCIP7, @RequestParam("quantite") int quantite);
 	
-	@ApiOperation(value = "Supprimer un produit du panier",
-			response = PresentationCardDTO.class)
-	@ApiResponse(responseCode = "200", description = "OK",
-			content = @Content(mediaType = "application/json", schema = @Schema(implementation = PresentationPanierDTO.class)))
-	@ApiResponse(responseCode = "500", description = "Erreur serveur",
-			content = @Content(mediaType = "application/json"))
-	@ApiResponse(responseCode = "404", description = "Commande ou présentation non trouvé dans la commande",
-			content = @Content(mediaType = "application/json"))
-	@DeleteMapping("/delete")
+	@ApiOperation(value = "Supprimer un produit du panier")
+	@ApiResponse(responseCode = "200", description = "OK")
+	@ApiResponse(responseCode = "500", description = "Erreur serveur")
+	@ApiResponse(responseCode = "404", description = "Commande ou présentation non trouvé dans la commande")
+	@DeleteMapping(value = "/delete", produces = "application/json")
 	ResponseEntity<Integer> deletePresentationToCart(@RequestParam("idUtilisateur") Long idUtilisateur,@RequestParam("codeCIP7") String codeCIP7);
 	
-	@ApiOperation(value = "Récupérer le panier de l'utilisateur",
-			response = PresentationCardDTO.class)
-	@ApiResponse(responseCode = "200", description = "OK",
-			content = @Content(mediaType = "application/json", schema = @Schema(implementation = PresentationPanierDTO.class)))
-	@ApiResponse(responseCode = "500", description = "Erreur serveur",
-			content = @Content(mediaType = "application/json"))
-	@ApiResponse(responseCode = "404", description = "Commande de l'utilisateur non trouvé",
-			content = @Content(mediaType = "application/json"))
-	@GetMapping("/panier")
+	@ApiOperation(value = "Récupérer le panier de l'utilisateur")
+	@ApiResponse(responseCode = "200", description = "OK")
+	@ApiResponse(responseCode = "500", description = "Erreur serveur")
+	@ApiResponse(responseCode = "404", description = "Commande de l'utilisateur non trouvé")
+	@GetMapping(value = "/panier", produces = "application/json")
 	ResponseEntity<List<PresentationPanierDTO>> getUserCart(@RequestParam("idUtilisateur") Long idUtilisateur);
 	
 }
--- restAPI/src/main/java/com/g2/gromed/endpoint/IPresentationEndpoint.java
+++ restAPI/src/main/java/com/g2/gromed/endpoint/IPresentationEndpoint.java
@@ -5,8 +5,6 @@ import com.g2.gromed.model.dto.presentation.Pagination;
 import com.g2.gromed.model.dto.presentation.PresentationCardDTO;
 import com.g2.gromed.model.dto.presentation.PresentationDetailDTO;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -16,26 +14,17 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/presentation")
 public interface IPresentationEndpoint {
 
-    @ApiOperation(value = "Récupère toutes les présentations des différents médicaments",
-            response = PresentationCardDTO.class)
-	@ApiResponse(responseCode = "200", description = "OK",
-			content = @Content(mediaType = "application/json", schema = @Schema(implementation = PresentationCardDTO.class)))
-	@ApiResponse(responseCode = "500", description = "Erreur serveur",
-			content = @Content(mediaType = "application/json"))
-	@ApiResponse(responseCode = "404", description = "Médicament non trouvé",
-			content = @Content(mediaType = "application/json"))
-    @GetMapping("/all")
+    @ApiOperation(value = "Récupère toutes les présentations des différents médicaments")
+	@ApiResponse(responseCode = "200", description = "OK")
+	@ApiResponse(responseCode = "500", description = "Erreur serveur")
+	@ApiResponse(responseCode = "404", description = "Médicament non trouvé")
+    @GetMapping(value = "/all", produces = "application/json")
     ResponseEntity<Page<PresentationCardDTO>> getPresentations(@ModelAttribute("pagination") Pagination pagination);
-	
-	
-	@ApiOperation(value = "Récupère les informations détallées d'une présentation d'un médicament",
-			response = PresentationDetailDTO.class)
-	@ApiResponse(responseCode = "200", description = "OK",
-			content = @Content(mediaType = "application/json", schema = @Schema(implementation = PresentationDetailDTO.class)))
-	@ApiResponse(responseCode = "500", description = "Erreur serveur",
-			content = @Content(mediaType = "application/json"))
-	@ApiResponse(responseCode = "404", description = "Médicament non trouvé",
-			content = @Content(mediaType = "application/json"))
-	@GetMapping("/detail")
+
+	@ApiOperation(value = "Récupère les informations détallées d'une présentation d'un médicament")
+	@ApiResponse(responseCode = "200", description = "OK")
+	@ApiResponse(responseCode = "500", description = "Erreur serveur")
+	@ApiResponse(responseCode = "404", description = "Médicament non trouvé")
+	@GetMapping(value = "/detail", produces = "application/json")
 	ResponseEntity<PresentationDetailDTO> getDetailPresentation(@RequestParam() String codeCIP7);
 }
