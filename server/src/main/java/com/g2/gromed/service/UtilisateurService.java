3d73118d9157140556ce407f9177b093ba5f5c64
jerome-gbz <jerome.gambiez@etu.univ-grenoble-alpes.fr>
2023-01-26 14:02:24 +0100

    fix utilisateur qui n'a pas de commande

--- server/src/main/java/com/g2/gromed/service/UtilisateurService.java
+++ server/src/main/java/com/g2/gromed/service/UtilisateurService.java
@@ -3,11 +3,14 @@ package com.g2.gromed.service;
 import com.g2.gromed.composant.CommandeComposant;
 import com.g2.gromed.composant.UtilisateurComposant;
 import com.g2.gromed.entity.Commande;
+import com.g2.gromed.entity.StatusCommande;
 import com.g2.gromed.entity.Utilisateur;
 import com.g2.gromed.mapper.IUtilisateurMapper;
 import com.g2.gromed.model.dto.utilisateur.UtilisateurDTO;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 @AllArgsConstructor
@@ -17,9 +20,18 @@ public class UtilisateurService {
 	private IUtilisateurMapper utilisateurMapper;
 	private CommandeComposant commandeComposant;
 	
-	public UtilisateurDTO authenticate(String email, String motDePasse){
+	public UtilisateurDTO authenticate(String email, String motDePasse) {
 		Utilisateur utilisateur = utilisateurComposant.authenticate(email, motDePasse);
 		Commande commande = commandeComposant.getCart(email);
+
+		if(commande == null) {
+			commande = new Commande();
+			commande.setUtilisateur(utilisateur);
+			commande.setStatus(StatusCommande.PANIER);
+			commande.setDateCommande(new Date());
+			commande = commandeComposant.createNewCommande(commande);
+		}
+
 		return utilisateurMapper.toUtilisateurDTO(utilisateur,commandeComposant.countCartPresentation(commande.getNumeroCommande()));
 	}
 }
