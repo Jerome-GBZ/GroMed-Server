e2a3c4aee0c60bce6b2710ef6d82e0177847b65c
= <=>
2023-01-26 15:13:17 +0100

    edj

--- server/src/main/java/com/g2/gromed/mapper/IPresentationMapper.java
+++ server/src/main/java/com/g2/gromed/mapper/IPresentationMapper.java
@@ -12,8 +12,8 @@ import java.util.List;
 @Mapper()
 public interface IPresentationMapper {
 
-	@Mapping(target="estGenerique",expression = "java(presentation.getMedicament().getGroupeGeneriques().isEmpty())")
-	@Mapping(target="contientInformation",expression = "java(presentation.getMedicament().getInfoImportantes().isEmpty())")
+	@Mapping(target="estGenerique",expression = "java(!presentation.getMedicament().getGroupeGeneriques().isEmpty())")
+	@Mapping(target="contientInformation",expression = "java(!presentation.getMedicament().getInfoImportantes().isEmpty())")
 	@Mapping(target="codeCIS",source="presentation.medicament.codeCIS")
 	@Mapping(target="titre",source="presentation.medicament.denomination")
 	@Mapping(target="description",source="presentation.denomination")
