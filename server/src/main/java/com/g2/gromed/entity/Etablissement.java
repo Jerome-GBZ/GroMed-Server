439c72bc44bbc5df24d8172f770effd01f5d9710
= <=>
2023-01-26 12:07:17 +0100

    Test jerem

---
+++ server/src/main/java/com/g2/gromed/entity/Etablissement.java
@@ -0,0 +1,40 @@
+package com.g2.gromed.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@IdClass(EtablissementId.class)
+@Table(name = "ETABLISSEMENT")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Etablissement {
+
+	@Id
+	private Long finess;
+
+	@Id
+	private String etalab;
+
+	private String nom;
+
+	private String telephone;
+
+	private String adresse;
+
+	private String codePostal;
+
+	private String ville;
+
+	private String departement;
+
+	private String region;
+
+	private String categorie;
+	
+}
---
+++ server/src/main/java/com/g2/gromed/entity/Utilisateur.java
@@ -0,0 +1,51 @@
+package com.g2.gromed.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "UTILISATEUR")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Utilisateur {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(unique = true)
+	private String email;
+
+	private String nom;
+
+	private String prenom;
+
+	private String motDePasse;
+
+	@Column(name = "date_naissance", columnDefinition = "DATE")
+	@Temporal(TemporalType.DATE)
+	private Date dateNaissance;
+
+	private String adresse;
+
+	private String codePostal;
+
+	private String ville;
+
+	private String telephone;
+
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumns(value = {@JoinColumn(name = "finess", referencedColumnName = "finess"), @JoinColumn(name = "etalab", referencedColumnName = "etalab")})
+	private Etablissement etablissement;
+	
+	@OneToMany(mappedBy = "utilisateur")
+	private List<Commande> commandes;
+
+	@OneToMany(mappedBy = "utilisateur")
+	private List<CommandeType> commandeTypes;
+}
