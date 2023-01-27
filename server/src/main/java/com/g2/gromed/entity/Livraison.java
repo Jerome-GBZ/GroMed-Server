e50a0dd34a65c6e60cd8f54dd640e1496fb484be
tomfr <tom.frances97@yahoo.fr>
2023-01-27 10:18:27 +0100

    remove unused import

--- server/src/main/java/com/g2/gromed/entity/Livraison.java
+++ server/src/main/java/com/g2/gromed/entity/Livraison.java
@@ -3,7 +3,6 @@ package com.g2.gromed.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import oracle.sql.TIMESTAMP;
 
 import java.util.Date;
 import java.util.List;
