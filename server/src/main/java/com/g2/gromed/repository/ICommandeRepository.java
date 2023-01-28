5fb085dcc2d8521f928923b9c961393912ac2e50
tomfr <tom.frances97@yahoo.fr>
2023-01-28 15:29:14 +0100

    correction requete

--- server/src/main/java/com/g2/gromed/repository/ICommandeRepository.java
+++ server/src/main/java/com/g2/gromed/repository/ICommandeRepository.java
@@ -6,10 +6,13 @@ import com.g2.gromed.entity.StatusCommande;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ICommandeRepository extends JpaRepository<Commande, Long> {
 	Commande findFirstByStatusAndUtilisateurEmail(StatusCommande panier, String email);
+
+	@Query("select c from Commande c join c.utilisateur u where u.email = :email and c.status in :status")
 	List<Commande> findByEmailAndStatusIn(String email, List<StatusCommande> status);
 }
