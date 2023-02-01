1a1dff165c4d75ef820b341e59ed4877a55f567d
= <=>
2023-02-01 09:27:44 +0100

    Correction endpoint presentationAll et Criteria pour ajout casse

--- server/src/main/java/com/g2/gromed/controller/PresentationController.java
+++ server/src/main/java/com/g2/gromed/controller/PresentationController.java
@@ -13,8 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.logging.Level;
-
 @CrossOrigin
 @RestController
 @Log
