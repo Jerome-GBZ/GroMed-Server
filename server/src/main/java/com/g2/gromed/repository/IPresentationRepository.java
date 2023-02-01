add391ae9d8df8b88cbd822ebe1a8d8667880c63
tomfr <tom.frances97@yahoo.fr>
2023-02-01 19:59:17 +0100

    requete recherche opti et fonctionnelle

--- server/src/main/java/com/g2/gromed/repository/IPresentationRepository.java
+++ server/src/main/java/com/g2/gromed/repository/IPresentationRepository.java
@@ -9,7 +9,10 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.LockModeType;
 import jakarta.persistence.QueryHint;
 import jakarta.persistence.criteria.*;
-import org.springframework.data.domain.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.QueryHints;
@@ -32,30 +35,46 @@ public interface IPresentationRepository extends JpaRepository<Presentation, Lon
         Join<Medicament, GroupeGenerique> groupeGeneriqueJoin = medicamentJoin.join("groupeGeneriques", JoinType.LEFT);
         Join<Medicament, Composition> compositionJoin = medicamentJoin.join("compositions", JoinType.LEFT);
 
+        CriteriaBuilder cbCount = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Long> cqCount = cbCount.createQuery(Long.class);
+        Root<Presentation> rootCount = cqCount.from(Presentation.class);
+        Join<Presentation, Medicament> medicamentJoinCount = rootCount.join("medicament");
+        Join<Medicament, GroupeGenerique> groupeGeneriqueJoinCount = medicamentJoinCount.join("groupeGeneriques", JoinType.LEFT);
+        Join<Medicament, Composition> compositionJoinCount = medicamentJoinCount.join("compositions", JoinType.LEFT);
+        cqCount.select(cbCount.countDistinct(rootCount.get("codeCIP7")));
+
         List<Predicate> predicateList = new ArrayList<>();
+        List<Predicate> predicateListCount = new ArrayList<>();
 
         if (!filtreDTO.getPresentationName().equals("")) {
             predicateList.add(cb.like(cb.lower(medicamentJoin.get("denomination")), "%" + filtreDTO.getPresentationName().toLowerCase() + "%"));
+            predicateListCount.add(cbCount.like(cbCount.lower(medicamentJoinCount.get("denomination")), "%" + filtreDTO.getPresentationName().toLowerCase() + "%"));
         }
 
         if (filtreDTO.isAvailable()) {
             predicateList.add(cb.greaterThan(root.get("stock"), 0));
+            predicateListCount.add(cbCount.greaterThan(rootCount.get("stock"), 0));
         }
 
         if (filtreDTO.getTitulaires() != null && !filtreDTO.getTitulaires().isEmpty()) {
             predicateList.add(medicamentJoin.get("titulaire").in(filtreDTO.getTitulaires()));
+            predicateListCount.add(medicamentJoinCount.get("titulaire").in(filtreDTO.getTitulaires()));
         }
 
         if (filtreDTO.isOriginal() && !filtreDTO.isGenerique()) {
             predicateList.add(cb.isNull(groupeGeneriqueJoin.get("medicament")));
+            predicateListCount.add(cbCount.isNull(groupeGeneriqueJoinCount.get("medicament")));
         }
 
         if (filtreDTO.isGenerique() && !filtreDTO.isOriginal()) {
             predicateList.add(cb.isNotNull(groupeGeneriqueJoin.get("medicament")));
+            predicateListCount.add(cbCount.isNotNull(groupeGeneriqueJoinCount.get("medicament")));
         }
 
         if (filtreDTO.getSubstancesDenomitations() != null && !filtreDTO.getSubstancesDenomitations().isEmpty()) {
             predicateList.add(compositionJoin.get("denominationSubstance").in(filtreDTO.getSubstancesDenomitations()));
+            predicateListCount.add(compositionJoinCount.get("denominationSubstance").in(filtreDTO.getSubstancesDenomitations()));
+
         }
 
         Sort sorts = pageable.getSort();
@@ -64,47 +83,11 @@ public interface IPresentationRepository extends JpaRepository<Presentation, Lon
         if(!predicateList.isEmpty()) {
             cq.where(predicateList.toArray(new Predicate[]{}));
         }
-        System.out.println("first = " + pageable.getPageNumber() * pageable.getPageSize());
-        System.out.println("max = " + pageable.getPageSize());
         List<Presentation> presentationList = entityManager
-                .createQuery(cq)
+                .createQuery(cq.distinct(true))
                 .setFirstResult(pageable.getPageNumber() * pageable.getPageSize())
                 .setMaxResults(pageable.getPageSize())
                 .getResultList();
-
-/*
-		CriteriaBuilder cbCount = entityManager.getCriteriaBuilder();
-		CriteriaQuery<Long> cqCount = cbCount.createQuery(Long.class);
-		Root<Presentation> rootCount = cqCount.from(Presentation.class);
-		Join<Presentation, Medicament> medicamentJoinCount = rootCount.join("medicament");
-		Join<Medicament, GroupeGenerique> groupeGeneriqueJoinCount = medicamentJoinCount.join("groupeGeneriques", JoinType.LEFT);
-		Join<Medicament, Composition> compositionJoinCount = medicamentJoinCount.join("compositions", JoinType.LEFT);
-		cqCount.select(cbCount.countDistinct(rootCount.get("codeCIP7")));
-		List<Predicate> predicateListCount = new ArrayList<>();
-
-		if (!filtreDTO.getPresentationName().equals("")) {
-			predicateListCount.add(cbCount.like(cbCount.lower(medicamentJoinCount.get("denomination")), "%" + filtreDTO.getPresentationName().toLowerCase() + "%"));
-		}
-
-		if (filtreDTO.isAvailable()) {
-			predicateListCount.add(cbCount.greaterThan(rootCount.get("stock"), 0));
-		}
-
-		if (filtreDTO.getTitulaires() != null && !filtreDTO.getTitulaires().isEmpty()) {
-			predicateListCount.add(medicamentJoinCount.get("titulaire").in(filtreDTO.getTitulaires()));
-		}
-
-		if (filtreDTO.isOriginal() && !filtreDTO.isGenerique()) {
-			predicateListCount.add(cbCount.isNull(groupeGeneriqueJoinCount.get("medicament")));
-		}
-
-		if (filtreDTO.isGenerique() && !filtreDTO.isOriginal()) {
-			predicateListCount.add(cbCount.isNotNull(groupeGeneriqueJoinCount.get("medicament")));
-		}
-
-		if (filtreDTO.getSubstancesDenomitations() != null && !filtreDTO.getSubstancesDenomitations().isEmpty()) {
-			predicateListCount.add(compositionJoinCount.get("denominationSubstance").in(filtreDTO.getSubstancesDenomitations()));
-		}
 
         if(!predicateListCount.isEmpty()) {
             cqCount.where(predicateListCount.toArray(new Predicate[]{}));
@@ -112,29 +95,13 @@ public interface IPresentationRepository extends JpaRepository<Presentation, Lon
 		Long count = entityManager
 				.createQuery(cqCount)
 				.getSingleResult();
-//
-//        int firstIndex = pageable.getPageNumber() * pageable.getPageSize();
-//        int lastIndex = firstIndex + pageable.getPageSize();
-//        int resultLast = presentationList.size();
-//
-//        if (firstIndex <= resultLast && lastIndex > resultLast) {
-//            lastIndex = resultLast;
-//        } else if (firstIndex > resultLast && lastIndex > resultLast) {
-//            firstIndex = 0;
-//            lastIndex = Math.min(presentationList.size(), pageable.getPageSize());
-//            pageable = PageRequest.of(0, pageable.getPageSize());
-//        }
 
-//        List<Presentation> result = presentationList.subList(firstIndex, lastIndex);*/
-
-        return new PageImpl<>(presentationList, pageable, 10);
+        return new PageImpl<>(presentationList, pageable, count);
     }
 
     private static void sortFunction(Sort sorts, CriteriaBuilder cb, CriteriaQuery<Presentation> cq, Root<Presentation> root, Join<Presentation, Medicament> medicamentJoin) {
-        String deno = "denomination";
-        String prix = "prix";
-        setSort(sorts, cb, cq, deno, medicamentJoin.get(deno));
-        setSort(sorts, cb, cq, prix, root.get(prix));
+        setSort(sorts, cb, cq, "denomination", medicamentJoin.get("denomination"));
+        setSort(sorts, cb, cq, "prix", root.get("prix"));
     }
 
     static void setSort(Sort sorts, CriteriaBuilder cb, CriteriaQuery<Presentation> cq, String deno, Path<Object> objectPath) {
